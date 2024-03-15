f7a91580ea85babaff6551d165972c59ad05a549
Anastasia <adwarica96r@email.here>
2024-03-15 18:28:38 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
+    ]);
+};
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, {
+                animal: "Cat",
+                name: "Thaksin",
+                breed: "Short-hair"
+            }),
+            React.createElement(Pet, {
+                animal: "Cat",
+                name: "Igby",
+                breed: "Short-hair"
+            }),
+            React.createElement(Pet, {
+                animal: "Dog",
+                name: "Pango",
+                breed: "German Shepherd"
+            }),
+
+        ]
+    );
+};
+
+
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
